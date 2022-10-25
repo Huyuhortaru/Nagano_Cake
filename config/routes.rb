f1c6ed818b76ae4db0cd9fa3b05e124178cf245f
Huyuhotaru <108530430+Huyuhortaru@users.noreply.github.com>
2022-10-25 15:00:41 +0000

    [Second]commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+
+ # 顧客用
+ # URL /customers/sign_in ...
+ devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+ }
+
+ # 管理者用
+ # URL /admin/sign_in ...
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+ }
+
   get 'users/show'
   get 'users/edit'
   get 'homes/top'
-  devise_for :users
-  devise_for :admins
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
